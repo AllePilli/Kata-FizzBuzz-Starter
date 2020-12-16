07b95c1b889f8b2012728dc73dba5fdad34f1a20
Martijn degrève <Martijn.degrve@edt.zone>
2020-12-16 12:14:48 +0100

    Updated java imports to new versions

--- kata-java-start/src/test/java/be/codekata/TestsShouldGoHere.java
+++ kata-java-start/src/test/java/be/codekata/TestsShouldGoHere.java
@@ -1,9 +1,9 @@
 package be.codekata;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
 
 public class TestsShouldGoHere {
 
